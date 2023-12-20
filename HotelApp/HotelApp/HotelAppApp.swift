af200149d0f379092e1dc1b2643d38fb3942c90e
dolludaa <dolontaeva00@gmail.com>
2023-12-20 12:21:44 -0500

    xcode project created

---
+++ HotelApp/HotelApp/HotelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelAppApp.swift
+//  HotelApp
+//
+//  Created by Людмила Долонтаева on 2023-12-20.
+//
+
+import SwiftUI
+
+@main
+struct HotelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
